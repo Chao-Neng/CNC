10a2e768d78f539d5c6e0a37ae168ee8209ddd93
CN-traveler <cn-traveler@outlook.com>
2023-09-03 16:01:42 +0800

    chore:项目初始化

---
+++ src/main/java/art/relev/springboot3/cnc/CNCApplication.java
@@ -0,0 +1,13 @@
+package art.relev.springboot3.cnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CNCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CNCApplication.class, args);
+    }
+
+}
