c4ce6eb7f761e43de6955d2a49f11b19670bfd73
CN-traveler <cn-traveler@outlook.com>
2023-09-03 17:02:09 +0800

    feat:统一异常处理

---
+++ src/main/java/art/relev/springboot3/cnc/controller/ExceptionController.java
@@ -0,0 +1,26 @@
+package art.relev.springboot3.cnc.controller;
+
+import art.relev.springboot3.cnc.exception.CNCException;
+import art.relev.springboot3.cnc.response.Result;
+import art.relev.springboot3.cnc.response.ResultMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(Exception.class)
+    public Result<?> exception(Exception exception) {
+        log.error("Exception: " + exception.getMessage());
+        return ResultMessage.ERROR.build();
+    }
+
+    @ExceptionHandler(CNCException.class)
+    public Result<?> appException(CNCException cncException) {
+        ResultMessage resultMessage = cncException.getResultMessage();
+        Result<?> result = resultMessage.build();
+        log.warn("CNCException: " + result.getMessage());
+        return result;
+    }
+}
